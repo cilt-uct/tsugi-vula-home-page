8ec88657ade72d46cb397b2e80eb8e4b463ef9a0
TurRil <lightscape2001@gmail.com>
2021-07-09 14:41:22 +0200

    TSUG-44: updated register.php

--- register.php
+++ register.php
@@ -1,10 +1,11 @@
 <?php
 
 $REGISTER_LTI2 = array(
-"name" => "VULA file Admin UI",
-"FontAwesome" => "fa-server",
-"short_name" => "VULA Admin UI",
-"description" => "A simple tool that allows a vula admin user to manage files.",
+    "name" => "VULA file Admin UI",
+    "FontAwesome" => "fa-server",
+    "short_name" => "VULA Admin UI",
+    "description" => "A simple tool that allows a admin user to manage files for the Vula homepage.",
+    
     // By default, accept launch messages..
     "messages" => array("launch"),
     "privacy_level" => "name_only",  // anonymous, name_only, public
@@ -12,7 +13,8 @@ $REGISTER_LTI2 = array(
     "languages" => array(
         "English"
     ),
-    "source_url" => "/tsugi/vula",
+    "tool_phase" => "new",
+    "source_url" => "https://github.com/cilt-uct/tsugi-vula-home-page",
     // For now Tsugi tools delegate this to /lti/store
     "placements" => array(
         /*
@@ -26,5 +28,4 @@ $REGISTER_LTI2 = array(
         "store/screen-01.PNG",
         "store/screen-02.PNG"
     )
-
 );
